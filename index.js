d51b417bc998d86bbad0e234fcfb9f04d18ceee4
thekoderbot <heckerman940@gmail.com>
2024-07-04 17:50:56 +0000

    Added sample .env, started postgres work, and performed some satanic rituals

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import 'dotenv/config';
 import { WebClient } from '@slack/web-api';
 import express from 'express';
+import pg from 'pg'
+const { Client } = pg
 
-// Read a token from the environment variables
 const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
+const client = new Client()
+await client.connect()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/commands/chat', (req, res) => {
+    web.chat.postEphemeral({
+        "channel": req.body.channel_id,
+        "user": req.body.user_id,
+        "text": ":spin-loading: Setting up chat..."
+    })
+    res.statusCode = 200;
+    res.send();
+});
+  
+app.listen(port, () => {
+    console.log(`Chattyy listening on port ${port}`)
+});
---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS chats (
+    id uuid PRIMARY KEY,
+    users text[] NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS users (
+    id text PRIMARY KEY,
+    chatStartTs text[] NOT NULL,
+    chatStartChannel text[] NOT NULL
+)

2166dae085ff131196a3eb8cc26b695a105102d2
thekoderbot <heckerman940@gmail.com>
2024-07-04 16:00:52 +0000

    setup express

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import 'dotenv/config'
-const { WebClient } = require('@slack/web-api');
+import 'dotenv/config';
+import { WebClient } from '@slack/web-api';
+import express from 'express';
 
 // Read a token from the environment variables
 const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
-
+const app = express()
+const port = process.env.PORT
